4d61366871ec3c3ca81393f6b85bd54ffe5996c3
KVN <78144196+kvnpp007ss@users.noreply.github.com>
2024-06-18 17:27:15 -0700

    Update Index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recetas Ecológicas</title>
+</head>
+<body>
+    <h1>Hola, Ian Carlo</h1>
+</body>
+</html>
